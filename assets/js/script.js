690868449509e129c9b037ab3eb357d6a41bede6
Vuqar <tu201906231@code.edu.az>
2022-03-18 23:38:19 +0400

    feat: Added Fancybox

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,3 +108,13 @@ AOS.init({
 //     delay: 10,
 //     time: 1200
 // })
+
+
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+    caption: function (fancybox, carousel, slide) {
+      return (
+        `${slide.index + 1} / ${carousel.slides.length} <br />` + slide.caption
+      );
+    },
+});
